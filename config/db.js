303d8246f14d762378f384fbc3be622f74c3b022
Ruslan <it.vasiliev@gmail.com>
2025-12-09 18:00:13 +0200

    chore: load mongodb uri from env

--- config/db.js
+++ config/db.js
@@ -7,12 +7,9 @@ if (!MONGODB_URI) {
     throw new Error('Missing MONGODB_URI environment variable');
 }
 
-// const URI =
-//     'mongodb+srv://mbglobalink_db_user:w5K8YTt85gb7gRo3@cluster0.fgrujud.mongodb.net/?appName=Cluster0';
 
 mongoose
     .connect(MONGODB_URI)
-    // .connect(URI)
     .then(() => {
         console.log('Connected to MongoDB');
     })
